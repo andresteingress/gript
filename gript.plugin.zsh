fc1feb43f66e4d02ce01494d27902082b76b4ffa
Andre Steingress <me@andresteingress.com>
2013-04-11 23:44:40 +0200

    added dir probing for ZSH

--- gript.plugin.zsh
+++ gript.plugin.zsh
@@ -1,6 +1,10 @@
-gript() {
+function "#"() {
     local function=$*[1]
     local args=$*[2,-1]
 
-    groovy '/Users/andre/.oh-my-zsh/custom/gript_script.groovy' "${function} ${args}"
+    if [[ -f "${ZSH}/plugins/gript/gript_script.groovy" ]]; then
+    	groovy "${ZSH}/plugins/gript/gript_script.groovy" "${function} ${args}"
+    else
+        groovy "${ZSH}/gript/gript_script.groovy" "${function} ${args}"	
+    fi
 }
